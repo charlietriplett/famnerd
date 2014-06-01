02bafc3396bb5f78c783f6661555104b497b7168
Charlie Triplett <charlietriplett@gmail.com>
2014-06-01 17:54:14 +0000

    Added coverline to chart

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,12 +8,14 @@ class Entry < ActiveRecord::Base
   # ensure that title is present and at least 10 chars long
   validates :temperature, presence: true
 
+  #adds up all of the values for this day
   def self.temperature(date)
     where("date(recorded_at_date) =?", date ).sum(:temperature)
   end
   
-  def self.id(date)
-    where("date(recorded_at_date) =?", date ).sum(:id)
+  def cover_line
+    cover_line = Entry.where(recorded_at_date: 6.days.ago..Time.now).maximum(:temperature)
+    cover_line + 0.1
   end
   
 end
